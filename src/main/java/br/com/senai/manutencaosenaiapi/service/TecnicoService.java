4a7668d3cea75c1d3c9b304c22609d4d16e2ae8c
Yago Matheus <yagofx@hotmail.com>
2022-04-07 20:35:05 -0300

    Adições de preconditions no tecnicoservice

--- src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
@@ -1,5 +1,7 @@
 package br.com.senai.manutencaosenaiapi.service;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 import com.google.common.base.Preconditions;
@@ -12,6 +14,18 @@ public class TecnicoService {
 	public Tecnico inserir(Tecnico novoTecnico) {
 		
 		Preconditions.checkNotNull(novoTecnico, "O técnico não pode ser nulo");
+		Preconditions.checkArgument(
+				novoTecnico.getNomeCompleto() != null && !novoTecnico.getNomeCompleto().isEmpty(),
+				"O nome é obrigatório");
+		Preconditions.checkArgument(novoTecnico.getDataDeAdmissao() != null, 
+				"A data de admissão é obrigatória");
+		
+		LocalDate dataAtual = LocalDate.now();
+		LocalDate dataDeAdmissao = novoTecnico.getDataDeAdmissao();
+		
+		Preconditions.checkArgument(dataDeAdmissao.isBefore(dataAtual)
+				|| dataDeAdmissao.isEqual(dataAtual),
+				"A data de admissão deve ser igual ou anterior a data atual");
 		
 		Tecnico tecnicoSalvo = novoTecnico;
 		return tecnicoSalvo;
