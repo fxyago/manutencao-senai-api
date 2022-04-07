35f5e104d0f0211919a94f7fd37891a54fa8bc0f
Yago Matheus <yagofx@hotmail.com>
2022-04-07 19:55:15 -0300

    Added equals and hashcode annotations

--- src/main/java/br/com/senai/manutencaosenaiapi/entity/Tecnico.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/entity/Tecnico.java
@@ -2,10 +2,13 @@ package br.com.senai.manutencaosenaiapi.entity;
 
 import java.time.LocalDate;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Tecnico {
 
+	@EqualsAndHashCode.Include
 	private Integer id;
 	private String nomeCompleto;
 	private LocalDate dataDeAdmissao;
