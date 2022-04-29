41e15386219b94bd1eb5f109c0ca498b461d4619
Yago Matheus <yagofx@hotmail.com>
2022-04-29 20:58:00 -0300

    Adicionado metodo deletarPor em TecnicosRepository

--- src/main/java/br/com/senai/manutencaosenaiapi/repository/TecnicosRepository.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/repository/TecnicosRepository.java
@@ -1,6 +1,8 @@
 package br.com.senai.manutencaosenaiapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
@@ -8,4 +10,8 @@ import br.com.senai.manutencaosenaiapi.entity.Tecnico;
 @Repository
 public interface TecnicosRepository extends JpaRepository<Tecnico, Integer> {
 
+	@Modifying
+	@Query(value = "DELETE FROM Tecnico t WHERE t.id = :id")
+	void deletarPor(Integer id);
+	
 }
