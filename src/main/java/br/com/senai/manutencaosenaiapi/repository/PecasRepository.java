2be8e5b4f5ff0bd1ace1271b5aa6043b0ae12d0f
Yago Matheus <yagofx@hotmail.com>
2022-04-29 20:29:37 -0300

    Adicionado método listarPorDescricao em pecas repository e substituido returns em peca service

--- src/main/java/br/com/senai/manutencaosenaiapi/repository/PecasRepository.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/repository/PecasRepository.java
@@ -1,6 +1,10 @@
 package br.com.senai.manutencaosenaiapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.senai.manutencaosenaiapi.entity.Peca;
@@ -8,6 +12,10 @@ import br.com.senai.manutencaosenaiapi.entity.Peca;
 @Repository
 public interface PecasRepository extends JpaRepository<Peca, Integer>{
 	
-	
+	@Query(value = 
+			"SELECT p "
+			+ "FROM Peca p "
+			+ "WHERE Upper(p.descricao) LIKE Upper(:desc)")
+	List<Peca> listarPor(@Param("desc") String descricao);
 	
 }
--- src/main/java/br/com/senai/manutencaosenaiapi/service/PecaService.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/PecaService.java
@@ -3,6 +3,7 @@ package br.com.senai.manutencaosenaiapi.service;
 import java.util.List;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -35,11 +36,19 @@ public class PecaService {
 		return repository.save(pecaSalva);
 	}
 	
+	public void removerPor(
+			@NotNull(message = "O id da peça para remoção não pode ser nulo")
+			@Min(value = 1, message = "O id da peça deve ser maior que zero")
+			Integer id) {
+		this.repository.deleteById(id);
+	}
+	
 	public List<Peca> listarPor(
 			@NotEmpty(message = "A descrição da busca é obrigatória")
 			@NotBlank(message = "A descrição não pode conter espaço em branco")
 			String descricao) {
-		return null;
+		return repository.listarPor(String.format("%%%s%%", descricao));
 	}
+	
 	
 }
