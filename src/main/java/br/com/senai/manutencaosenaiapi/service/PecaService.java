d83abadd0fdd5094c949bdd2cec0b2ecc658906a
Yago Matheus <yagofx@hotmail.com>
2022-04-28 21:52:57 -0300

    Linkado repository de peça em sua service

--- src/main/java/br/com/senai/manutencaosenaiapi/service/PecaService.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/PecaService.java
@@ -1,6 +1,5 @@
 package br.com.senai.manutencaosenaiapi.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -8,37 +7,39 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import br.com.senai.manutencaosenaiapi.entity.Peca;
+import br.com.senai.manutencaosenaiapi.repository.PecasRepository;
 
 @Service
 @Validated
 public class PecaService {
 
+	@Autowired
+	private PecasRepository repository;
+	
 	public Peca inserir(
 			@Valid 
 			@NotNull(message = "A peça não pode ser nula")
 			Peca novaPeca) {
-		Peca pecaSalva = novaPeca;
-		return pecaSalva;
+		return repository.save(novaPeca);
 	}
 	
 	public Peca alterar(
 			@Valid 
 			@NotNull(message = "A peça não pode ser nula")
 			Peca pecaSalva) {
-		Peca pecaAtualizada = pecaSalva;
-		
-		return pecaAtualizada;
+		return repository.save(pecaSalva);
 	}
 	
 	public List<Peca> listarPor(
 			@NotEmpty(message = "A descrição da busca é obrigatória")
 			@NotBlank(message = "A descrição não pode conter espaço em branco")
 			String descricao) {
-		return new ArrayList<Peca>();
+		return null;
 	}
 	
 }
