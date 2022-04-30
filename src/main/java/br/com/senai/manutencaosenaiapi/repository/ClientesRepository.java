1e09cfc0da181fb66674560899654116c36bf391
Yago Matheus <yagofx@hotmail.com>
2022-04-29 21:42:27 -0300

    Criado ClientesRepository

---
+++ src/main/java/br/com/senai/manutencaosenaiapi/repository/ClientesRepository.java
@@ -0,0 +1,26 @@
+package br.com.senai.manutencaosenaiapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.senai.manutencaosenaiapi.entity.Cliente;
+
+@Repository
+public interface ClientesRepository extends JpaRepository<Cliente, Integer> {
+
+	@Modifying
+	@Query(value = "DELETE FROM Cliente c WHERE c.id = :id")
+	void deletarPor(Integer id);
+
+	@Query(value = 
+			"SELECT c "
+			+ "FROM Cliente c "
+			+ "WHERE Upper(c.nome) LIKE Upper(:nome)")
+	List<Cliente> listarPor(@Param("nome") String nome);
+	
+}
