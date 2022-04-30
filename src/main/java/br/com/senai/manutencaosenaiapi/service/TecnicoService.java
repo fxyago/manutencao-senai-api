2f16f3d0f7d3677e4b981e3a35da674968225fbd
Yago Matheus <yagofx@hotmail.com>
2022-04-29 21:43:20 -0300

    Finalizado Tecnico Service e Repository

--- src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/service/TecnicoService.java
@@ -1,6 +1,5 @@
 package br.com.senai.manutencaosenaiapi.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -9,24 +8,30 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
 import com.google.common.base.Preconditions;
 
 import br.com.senai.manutencaosenaiapi.entity.Tecnico;
+import br.com.senai.manutencaosenaiapi.repository.TecnicosRepository;
 
 @Service
 @Validated
 public class TecnicoService {
 
+	@Autowired
+	private TecnicosRepository repository;
+	
 	public Tecnico inserir(
 			@Valid
 			@NotNull(message = "O técnico não pode ser nulo")
 			Tecnico novoTecnico) {			
 		Preconditions.checkArgument(novoTecnico.isNovo(),
 				"O técnico já foi salvo");
-		Tecnico tecnicoSalvo = novoTecnico;	
+		Tecnico tecnicoSalvo = this.repository.save(novoTecnico);	
 		return tecnicoSalvo;				
 	}
 	
@@ -36,7 +41,7 @@ public class TecnicoService {
 			Tecnico tecnicoSalvo) {
 		Preconditions.checkArgument(!tecnicoSalvo.isNovo(), 
 				"O técnico ainda não foi inserido");
-		Tecnico tecnicoAtualizado = tecnicoSalvo;
+		Tecnico tecnicoAtualizado = this.repository.save(tecnicoSalvo);
 		return tecnicoAtualizado;
 	}
 	
@@ -44,16 +49,23 @@ public class TecnicoService {
 			@NotEmpty(message = "O nome para busca não pode ser nulo")
 			@NotBlank(message = "Não pode haver espaços antes do nome")
 			String nome) {
-		return new ArrayList<Tecnico>();
+		return this.repository.listarPor(String.format("%%%s%%", nome));
 	}
 	
+	@Transactional
 	public void removerPor(
 			@NotNull(message = "O id de exclusão não pode ser nulo")
 			@Min(value = 1, message = "O id deve ser maior que zero")
 			Integer id) {
-		//TODO: implementar lógica de remoção
+		this.repository.deleteById(id);
 	}
 	
+	public Tecnico buscarPor(
+			@NotNull(message = "O id de busca não pode ser nulo")
+			@Min(value = 1, message = "O id deve ser maior que zero")
+			Integer id) {
+		return this.repository.findById(id).get();
+	}
 	
 	
 }
